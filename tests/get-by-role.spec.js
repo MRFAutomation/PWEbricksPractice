f456fc432f0f79870f146b3aecfa234e2f869b1d
MRFAutomation <mrf.testdevice@gmail.com>
2024-05-21 12:07:48 +0500

    Day8 PW Assignment GetByRole

---
+++ tests/get-by-role.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test("Use of getByRole function in 5 different ways", async ({ page }) => {
+    await page.goto("https://www.letskodeit.com/practice");
+
+    await expect(page.getByRole('heading', { name: 'Practice Page' })).toBeVisible();
+    await page.getByRole('radio').first().click();
+    await page.getByRole('checkbox').nth(1).click();
+    await page.getByRole('link', { name: 'Open Tab' }).click();
+    await page.getByRole('button', { name: 'Open Window' }).click();
+
+    await page.waitForTimeout(3000);
+})
