c0fe98e46e3804686382d9b55eaf5ee87f3c18ec
MRFAutomation <mrf.testdevice@gmail.com>
2024-05-28 11:50:01 +0500

    Day 12 PW Assignment Hooks

---
+++ tests/day12-hooks.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+
+test.describe('Describe block', () => {
+
+    test.beforeAll('beforeAll in describe block', () => console.log('beforeAll in describe block'));
+
+    test('Test 1 in describe block', () => console.log('Test 1 inside describe block'));
+    test('Test 2 in describe block', () => console.log('Test 2 inside describe block'));
+
+    test.afterAll('afterAll in describe block', () => console.log('afterAll in describe block'));
+})
+test.beforeAll('beforeAll outside describe block', () => console.log('beforeAll outside describe block'));
+
+test.beforeEach('beforeEach outside describe block', () => console.log('beforeEach outside describe block'));
+
+test('Test 1 outside describe block', () => console.log('Test 1 outside describe block'));
+test('Test 2 outside describe block', () => console.log('Test 2 outside describe block'));
+test('Test 3 outside describe block', () => console.log('Test 3 outside describe block'));
+
+test.afterEach('afterEach outside describe block', () => console.log('afterEach outside describe block'));
+
+test.afterAll('afterAll outside describe block', () => { console.log('afterAll outside describe block') });
