397067b4490497c757b473ed05d7a3d07121314b
MRFAutomation <mrf.testdevice@gmail.com>
2024-05-28 13:38:30 +0500

    Day 12 PW Assignment Hooks

--- tests/day12-hooks.spec.js
+++ tests/day12-hooks.spec.js
@@ -10,8 +10,8 @@ test.describe('Describe block', () => {
 
     test.afterAll('afterAll inside describe block', () => console.log('afterAll inside describe block'));
 })
-test.beforeAll('beforeAll outside describe block', () => console.log('beforeAll outside describe block'));
 
+test.beforeAll('beforeAll outside describe block', () => console.log('beforeAll outside describe block'));
 test.beforeEach('beforeEach outside describe block', () => console.log('beforeEach outside describe block'));
 
 test('Test 1 outside describe block', () => console.log('Test 1 outside describe block'));
@@ -19,5 +19,4 @@ test('Test 2 outside describe block', () => console.log('Test 2 outside describe
 test('Test 3 outside describe block', () => console.log('Test 3 outside describe block'));
 
 test.afterEach('afterEach outside describe block', () => console.log('afterEach outside describe block'));
-
 test.afterAll('afterAll outside describe block', () => { console.log('afterAll outside describe block') });
