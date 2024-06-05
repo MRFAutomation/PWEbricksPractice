9b332933cef3461e716b8446a72732768c8d1666
MRFAutomation <mrf.testdevice@gmail.com>
2024-06-05 11:04:30 +0500

    Day 17 PW Login JS data file

---
+++ data/day17LoginData.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        "testId": "1",
+        webURL: "https://ecommerce-playground.lambdatest.io",
+        email: "mrf.testdevice@gmail.com",
+        password: "sXSDaE6xON1LR",
+        "loginHeading": "My Account",
+        "errorMsgCredentialsNotMatched": " Warning: No match for E-Mail Address and/or Password.",
+        "errortxt": " Warning: No match for E-Mail Address and/or Password.",
+        "loginLimitExceed": "Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour."
+    },
+    {
+        "testId": "2",
+        webURL: "https://ecommerce-playground.lambdatest.io",
+        email: "mrf.testdevice@gmail.com",
+        password: "sXSDaE6xON1LR",
+        "loginHeading": "My Account",
+        "errorMsgCredentialsNotMatched": " Warning: No match for E-Mail Address and/or Password.",
+        "errortxt": " Warning: No match for E-Mail Address and/or Password.",
+        "loginLimitExceed": "Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour."
+    },
+    {
+        "testId": "3",
+        webURL: "https://ecommerce-playground.lambdatest.io",
+        email: "mrf.testdevice@gmail.com",
+        password: "sXSDaE6xON1LR",
+        "loginHeading": "My Account",
+        "errorMsgCredentialsNotMatched": " Warning: No match for E-Mail Address and/or Password.",
+        "errortxt": " Warning: No match for E-Mail Address and/or Password.",
+        "loginLimitExceed": "Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour."
+    },
+    {
+        "testId": "4",
+        webURL: "https://ecommerce-playground.lambdatest.io",
+        email: "mrf.testdevice@gmail.com",
+        password: "sXSDaE6xON1LR",
+        "loginHeading": "My Account",
+        "errorMsgCredentialsNotMatched": " Warning: No match for E-Mail Address and/or Password.",
+        "errortxt": " Warning: No match for E-Mail Address and/or Password.",
+        "loginLimitExceed": "Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour."
+    },
+    {
+        "testId": "5",
+        webURL: "https://ecommerce-playground.lambdatest.io",
+        email: "mrf.testdevice@gmail.com",
+        password: "sXSDaE6xON1LR",
+        "loginHeading": "My Account",
+        "errorMsgCredentialsNotMatched": " Warning: No match for E-Mail Address and/or Password.",
+        "errortxt": " Warning: No match for E-Mail Address and/or Password.",
+        "loginLimitExceed": "Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour."
+    }
+]
