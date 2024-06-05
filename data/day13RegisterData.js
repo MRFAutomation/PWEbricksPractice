fe2ed1d8c13ba2b098c4caae66216b88a6dcfee6
MRFAutomation <mrf.testdevice@gmail.com>
2024-06-05 11:00:24 +0500

    Day 13 PW Register data file

---
+++ data/day13RegisterData.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "url": "https://ecommerce-playground.lambdatest.io",
+    "firstName": "Rana",
+    "lastName": "Farhan",
+    "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+    "tPhone": "12345678900",
+    "password": "sXSDaE6xON1LR",
+    "assertText": "Your Account Has Been Created!"
+}
