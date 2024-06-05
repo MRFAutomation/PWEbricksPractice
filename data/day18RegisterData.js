677771f27a429a1d8b76d29262ab832e10f4a191
MRFAutomation <mrf.testdevice@gmail.com>
2024-06-05 11:57:19 +0500

    Day 18 PW Register JS data file

---
+++ data/day18RegisterData.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+
+        "url": "https://ecommerce-playground.lambdatest.io",
+        "firstName": "Rana",
+        "lastName": "Farhan",
+        "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+        "tPhone": "12345678900",
+        "password": "sXSDaE6xON1LR",
+        "assertText": "Your Account Has Been Created!"
+
+    },
+    {
+
+        "url": "https://ecommerce-playground.lambdatest.io",
+        "firstName": "Rana",
+        "lastName": "Farhan",
+        "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+        "tPhone": "12345678900",
+        "password": "sXSDaE6xON1LR",
+        "assertText": "Your Account Has Been Created!"
+
+    },
+    {
+
+        "url": "https://ecommerce-playground.lambdatest.io",
+        "firstName": "Rana",
+        "lastName": "Farhan",
+        "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+        "tPhone": "12345678900",
+        "password": "sXSDaE6xON1LR",
+        "assertText": "Your Account Has Been Created!"
+
+    }
+]
