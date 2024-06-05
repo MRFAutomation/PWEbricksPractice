3ed6a8f69d6ff42ebdd57707e6169566e8e53ef5
MRFAutomation <mrf.testdevice@gmail.com>
2024-06-05 11:55:52 +0500

    Day 17 PW Register JS data file

---
+++ data/day17RegisterData.js
@@ -0,0 +1,57 @@
+module.exports = [
+    {
+        "testId": "1",
+        "url": "https://ecommerce-playground.lambdatest.io",
+        "firstName": "Rana",
+        "lastName": "Farhan",
+        "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+        "tPhone": "12345678900",
+        "password": "sXSDaE6xON1LR",
+        "btnTxt": "I have read and agree to the",
+        "assertText": "Your Account Has Been Created!"
+    },
+    {
+        "testId": "2",
+        "url": "https://ecommerce-playground.lambdatest.io",
+        "firstName": "Rana",
+        "lastName": "Farhan",
+        "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+        "tPhone": "12345678900",
+        "password": "sXSDaE6xON1LR",
+        "btnTxt": "I have read and agree to the",
+        "assertText": "Your Account Has Been Created!"
+    },
+    {
+        "testId": "3",
+        "url": "https://ecommerce-playground.lambdatest.io",
+        "firstName": "Rana",
+        "lastName": "Farhan",
+        "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+        "tPhone": "12345678900",
+        "password": "sXSDaE6xON1LR",
+        "btnTxt": "I have read and agree to the",
+        "assertText": "Your Account Has Been Created!"
+    },
+    {
+        "testId": "4",
+        "url": "https://ecommerce-playground.lambdatest.io",
+        "firstName": "Rana",
+        "lastName": "Farhan",
+        "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+        "tPhone": "12345678900",
+        "password": "sXSDaE6xON1LR",
+        "btnTxt": "I have read and agree to the",
+        "assertText": "Your Account Has Been Created!"
+    },
+    {
+        "testId": "5",
+        "url": "https://ecommerce-playground.lambdatest.io",
+        "firstName": "Rana",
+        "lastName": "Farhan",
+        "email": (Math.random() + 1).toString(36).substring(7) + "@gmail.com",
+        "tPhone": "12345678900",
+        "password": "sXSDaE6xON1LR",
+        "btnTxt": "I have read and agree to the",
+        "assertText": "Your Account Has Been Created!"
+    },
+]
